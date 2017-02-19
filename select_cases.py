8fe875dacec9de4cda2e387be6a9369c657c455e
vallisu <syslik110@gmail.com>
2017-02-19 14:15:46 +0300

    Доделано создание нового файла (old_file_name + _res); Запись в новый файл рандомных строк из старого файла

--- select_cases.py
+++ select_cases.py
@@ -5,6 +5,8 @@
 # По умолчанию необходимо выбрать 10 строк.
 
 import os
+import random
+import re
 
 # Enter path
 path_to_file = input('Enter the path where .txt file is located: ')
@@ -26,6 +28,21 @@ number_of_strings = number_of_strings or 10
 
 # Open file
 file = open(path_to_file)
-print(file.read())
+
+# Create and open new file with "_res" using re
+(path, filename) = os.path.split(path_to_file)
+new_file_res = re.sub(r".txt", r"_res.txt", filename)
+file_res = os.path.join(path, new_file_res)
+file_res = open(file_res, 'w')
+
+lines = file.readlines()
+strings = len(lines)
+
+file_res.write(lines[0])
+for n in range(number_of_strings):
+    file_res.write(lines[random.randint(1, strings-1)] + '\n')
 
 file.close()
+file_res.close()
+
+print("Done. Check the created file near the parent file is located. ")
