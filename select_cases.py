96f6c090c6f0c24a67173285785903d56fa37ace
vallisu <syslik110@gmail.com>
2017-04-15 13:35:16 +0300

    changes

--- select_cases.py
+++ select_cases.py
@@ -10,7 +10,6 @@
 #    с путем и расширением исходного файла, а имя формируется как имя исходного файла + «_res»;
 # 4) Сохранить исходный файл без выбранных строк;
 # 5) Вернуть необходимо путь к результирующему файлу.
-
 import os
 import random
 import re
@@ -20,59 +19,59 @@ path_to_file = input('Enter the path where .txt file is located: ')
 while not os.path.exists(path_to_file):
     path_to_file = input('The entered path to file is incorrect. Try again: ')
 
+# Create and open new file with "_res" using re
+(path, filename) = os.path.split(path_to_file)
+new_file_res = re.sub(r".txt", r"_res.txt", filename)
+file_res_path = os.path.join(path, new_file_res)
+
 # Open the file for count a number of strings
 file = open(path_to_file)
 lines = file.readlines()
 file.close()
 
 # Enter a count of strings
+counter = 0
+max_counter = 5
 while True:
     try:
-        number_of_strings = input('Enter a valid number of strings not more than ' + str(len(lines)-1) + ' : ')
+        if counter == max_counter:
+            print('You spent your attempts for entering the correct value. The program will closed.')
+            exit(0)
+        number_of_strings = input('Enter a valid number of strings not more than ' + str(len(lines) - 1) + ' : ')
+        counter += 1
         if not number_of_strings:
             break
         number_of_strings = int(number_of_strings)
-        while number_of_strings > len(lines) - 1:
-            number_of_strings = input(
-                'Enter a valid number of strings not more than ' + str(len(lines)-1) + ' : ')
-            number_of_strings = int(number_of_strings)
+        if number_of_strings < len(lines):
+            break
+        else:
+            continue
     except ValueError:
         continue
-    break
 number_of_strings = number_of_strings or 10
 
 
-def select_cases(path_to_file, number_of_strings):
+def select_cases(path_to_file, number_of_strings, file_res_path):
     # Open file
     file = open(path_to_file, 'r')
-
-    # Create and open new file with "_res" using re
-    (path, filename) = os.path.split(path_to_file)
-    new_file_res = re.sub(r".txt", r"_res.txt", filename)
-    file_res_path = os.path.join(path, new_file_res)
-    file_res = open(file_res_path, 'w+')
-    
+    file_res = open(file_res_path, 'w')
     # Strings from file -> to list
     lines = file.readlines()
     file.close()
     strings = len(lines)
-
     # Select string by random, push it to new file and remove from list
     file_res.write(lines[0])
     for n in range(number_of_strings):
         rand = random.randint(1, strings - 1)
-        file_res.write(lines[rand] + '\n')
+        file_res.write(lines[rand])
         lines.pop(rand)
-        strings = strings - 1
-
+        strings -= 1
     file = open(path_to_file, 'w')
     for i in lines:
-        file.write(i + '\n')
+        file.write(i)
     file.close()
     file_res.close()
-
     return os.path.abspath(file_res_path)
 
-
-res = select_cases(path_to_file, number_of_strings)
+res = select_cases(path_to_file, number_of_strings, file_res_path)
 print("Path for your file with test cases: " + res)
